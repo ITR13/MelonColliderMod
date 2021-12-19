3545c3202dbf9ddab987db59272b978ee481a4af
ITR <ITR@live.no>
2021-12-19 21:53:12 +0100

    v1.1.2

--- ColliderMod/Properties/AssemblyInfo.cs
+++ ColliderMod/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using MelonLoader;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: MelonInfo(typeof(MainClass), "ITR's Collider Mod", "1.1.1", "ITR")]
+[assembly: MelonInfo(typeof(MainClass), "ITR's Collider Mod", "1.1.2", "ITR")]
 [assembly: MelonGame(null, null)]
